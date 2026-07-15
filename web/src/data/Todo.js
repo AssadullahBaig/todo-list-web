cfc95387643026064fa817d1721ef64edf268a36
Assadullah Baig <assadullahbaig3@gmail.com>
2026-07-15 09:24:30 +0500

    feat: add todo feature folder structure

---
+++ web/src/data/Todo.js
@@ -0,0 +1,22 @@
+const todos = [
+  {
+    id: 1,
+    title: "Learn React",
+    completed: true,
+    dueDate: "Today",
+  },
+  {
+    id: 2,
+    title: "Finish Assignment",
+    completed: false,
+    dueDate: "Tomorrow",
+  },
+  {
+    id: 3,
+    title: "Go to Gym",
+    completed: false,
+    dueDate: "Today",
+  },
+];
+
+export default todos;
