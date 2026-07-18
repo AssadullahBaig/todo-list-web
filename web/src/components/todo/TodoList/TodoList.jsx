ab863bfe05903e7b4da94d3a0f9069f6dbffa75f
Assadullah Baig <assadullahbaig3@gmail.com>
2026-07-18 11:43:34 +0500

    refactor: receive todos through component props

--- web/src/components/todo/TodoList/TodoList.jsx
+++ web/src/components/todo/TodoList/TodoList.jsx
@@ -1,13 +1,9 @@
 import TodoCard from "../TodoCard";
 
 function TodoList({ todos }) {
-  return (
-    <section className="space-y-4">
-      {todos.map((todo) => (
-        <TodoCard key={todo.id} todo={todo} />
-      ))}
-    </section>
-  );
+  const todoCards = todos.map((todo) => <TodoCard key={todo.id} todo={todo} />);
+
+  return <section className="space-y-4">{todoCards}</section>;
 }
 
 export default TodoList;
